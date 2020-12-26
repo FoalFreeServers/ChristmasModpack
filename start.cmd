5c10627e2eb3f29ab4cf705d9bd465a0e090135c
Sydney <github@sweetiebelle.org>
2020-12-26 17:36:57 -0500

    Sync from Server

--- start.cmd
+++ start.cmd
@@ -1,4 +1,4 @@
 :loop
-java -jar -Xmx8G -XX:-OmitStackTraceInFastThrow forge-1.16.4-35.1.18.jar nogui
+java -jar -Xmx8G -XX:-OmitStackTraceInFastThrow -Dmixin.debug.export=true -Dmixin.debug.verbose=true forge-1.16.4-35.1.18.jar nogui
 timeout 5
 goto loop
