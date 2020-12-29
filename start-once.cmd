2ae2aaac8a005a9b27716a73a2601e6cdfaa1898
Sydney <github@sweetiebelle.org>
2020-12-29 12:59:29 -0500

    Fix startup scripts.

--- start-once.cmd
+++ start-once.cmd
@@ -1,1 +1,1 @@
-java -jar -Xmx8G -XX:-OmitStackTraceInFastThrow -Dmixin.debug=true forge-1.16.4-35.1.24.jar nogui
+java -jar -Xmx8G -XX:-OmitStackTraceInFastThrow -Dmixin.debug.export=true -Dmixin.debug.verbose=true forge-1.16.4-35.1.24.jar nogui
