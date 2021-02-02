1716f51d063bfb7087d528dbaae549817fd9bdd8
Sydney <github@sweetiebelle.org>
2021-02-02 14:24:47 -0500

    Sync from Server

--- start.cmd
+++ start.cmd
@@ -1,4 +1,4 @@
 :loop
-java -jar -Xmx12G -XX:-OmitStackTraceInFastThrow -Dlog4j.configurationFile=log4j2.xml -Dmixin.debug.export=true -Dmixin.debug.verbose=true forge-1.16.5-36.1.2-shroom-1.16.x.jar nogui
+java -jar -Xmx12G -XX:-OmitStackTraceInFastThrow -Dlog4j.configurationFile=log4j2.xml -Dmixin.debug.export=true -Dmixin.debug.verbose=true forge-1.16.5-36.1.12-shroom-1.16.x.jar nogui
 timeout 5
 goto loop
